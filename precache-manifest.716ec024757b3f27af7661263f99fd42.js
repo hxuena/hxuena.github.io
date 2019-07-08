91ffc7e3f820c0aaedf66ede154f9a5f73c6b3da
xnhao <xnhao@ifltek.com>
2019-07-08 09:34:46 +0000

    update

---
+++ precache-manifest.716ec024757b3f27af7661263f99fd42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ab4d7fe8784faef08620f1bb19c140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32dbf7cbcb965d8f97c2",
+    "url": "/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "e17e744eccbf0d689e11",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "32dbf7cbcb965d8f97c2",
+    "url": "/static/js/2.9f0c3b0e.chunk.js"
+  },
+  {
+    "revision": "e17e744eccbf0d689e11",
+    "url": "/static/js/main.3655c958.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
